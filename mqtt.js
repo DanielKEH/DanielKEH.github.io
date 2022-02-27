d6bb77e4e378b63664a88610715013f935ca653d
DanielKEH <86837462+DanielKEH@users.noreply.github.com>
2022-02-27 13:44:32 +0100

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -1,11 +1,7 @@
 
-
-
 var mqtt_server = "test.mosquitto.org";
 var mqtt_port = "8081";
-var mqtt_destname = "DanielLed";
-
-
+var mqtt_destname = "DanielMqtt";
 
 function conectMQTT() {
     client = new Paho.MQTT.Client(mqtt_server, Number(mqtt_port), "");
@@ -18,18 +14,31 @@ function conectMQTT() {
 
 function onConnect() {
     document.getElementById("pubmsg").innerHTML = "New connection made...";
-    document.getElementById("sendButton").style.visibility = "visible";
+    document.getElementById("startButton").style.visibility = "visible";
+    document.getElementById("stopButton").style.visibility = "visible";
+    document.getElementById("sliderRng").style.visibility = "visible";
 }
 
-function send_mqtt_msg() {
-    var mqtt_msg = document.getElementById("mqtt_msg").value;
+function setSpeed() {
+    var mqtt_msg = document.getElementById("sliderRng").value;
     message = new Paho.MQTT.Message(mqtt_msg);
     message.destinationName = mqtt_destname;
     client.send(message);
     document.getElementById("pubmsg").innerHTML = mqtt_msg + " was sent to : " + mqtt_destname;
 }
 
-function changeButtonValue() {
+function startMot() {
+    var mqtt_msg = "1000";
+    message = new Paho.MQTT.Message(mqtt_msg);
+    message.destinationName = mqtt_destname;
+    client.send(message);
+    document.getElementById("pubmsg").innerHTML = mqtt_msg + " was sent to : " + mqtt_destname;
 
-    document.getElementById("sendButton").innerHTML = "Publish " + document.getElementById("mqtt_msg").value + " to MQTT";
+}
+function stopMot() {
+    var mqtt_msg = "1001";
+    message = new Paho.MQTT.Message(mqtt_msg);
+    message.destinationName = mqtt_destname;
+    client.send(message);
+    document.getElementById("pubmsg").innerHTML = mqtt_msg + " was sent to : " + mqtt_destname;
 }
